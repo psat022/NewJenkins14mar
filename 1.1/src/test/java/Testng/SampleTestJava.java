f0b5a02b774b5ac8f15ec2da3047bfd8889bd42b
psat022 <prashant022satpute@gmail.com>
2025-03-14 16:48:35 +0530

    push1

---
+++ 1.1/src/test/java/Testng/SampleTestJava.java
@@ -0,0 +1,13 @@
+package Testng;
+
+import org.testng.annotations.Test;
+
+public class SampleTestJava {
+
+	
+	@Test
+	public void createProduct()
+	{
+		System.out.println("Product is created");
+	}
+}
