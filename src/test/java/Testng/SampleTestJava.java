731ca60c54aaea96c81d70cd636145d448a92f35
psat022 <prashant022satpute@gmail.com>
2025-03-14 16:53:08 +0530

    puushing1

---
+++ src/test/java/Testng/SampleTestJava.java
@@ -0,0 +1,14 @@
+package Testng;
+
+import org.testng.annotations.Test;
+
+public class SampleTestJava {
+
+	
+	@Test
+	//execution1
+	public void createProduct()
+	{
+		System.out.println("Product is created");
+	}
+}
